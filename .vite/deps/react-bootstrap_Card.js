f3eeb2d51cf56dbf6d879d2c3bbddd474336f68c
Rosiita Garcia <rosadgarcia91@gmail.com>
2025-06-08 20:28:43 -0400

    Cambios

---
+++ .vite/deps/react-bootstrap_Card.js
@@ -0,0 +1,9 @@
+"use client";
+import {
+  Card_default
+} from "./chunk-PVOC5NZY.js";
+import "./chunk-SQC3XEDQ.js";
+import "./chunk-GT27OAPC.js";
+export {
+  Card_default as default
+};
---
+++ .vite/deps/react-bootstrap_Form.js
@@ -0,0 +1,8 @@
+import {
+  Form_default
+} from "./chunk-G5ESBUPU.js";
+import "./chunk-SQC3XEDQ.js";
+import "./chunk-GT27OAPC.js";
+export {
+  Form_default as default
+};
